19be0062efee6f6d51c4ba96f5ed7f5036586c28
sphassan <sphassan@sbcglobal.net>
2017-06-20 15:36:57 -0500

    Gradle tweaks

--- team-null/app/src/main/java/app/anull/net/team_null/GameActivity.java
+++ team-null/app/src/main/java/app/anull/net/team_null/GameActivity.java
@@ -1,12 +1,9 @@
 package app.anull.net.team_null;
 
-import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
 
 public class GameActivity extends AppCompatActivity {
 
