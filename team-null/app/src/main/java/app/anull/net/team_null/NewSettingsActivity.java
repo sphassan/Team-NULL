cf7a12b6b0199575e8adb80394763e3efd37f414
beaverdabeaver <sukuraz12@gmail.com>
2017-06-21 14:59:34 -0500

    Created the new settings layout and activity

--- team-null/app/src/main/java/app/anull/net/team_null/NewSettingsActivity.java
+++ team-null/app/src/main/java/app/anull/net/team_null/NewSettingsActivity.java
@@ -1,7 +1,9 @@
 package app.anull.net.team_null;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class NewSettingsActivity extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class NewSettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_settings);
+        setupActionBar();
+    }
+
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Play");
+        if (actionBar != null) {
+            // Show the Up button in the action bar.
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                super.onBackPressed();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
