3c06c73139dc74f60e4d74505f3a256bf7328e4f
beaverdabeaver <sukuraz12@gmail.com>
2017-06-21 14:32:16 -0500

    Changed intent class for settings to new settings

---
+++ team-null/app/src/main/gen/app/anull/net/team_null/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package app.anull.net.team_null;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
--- team-null/app/src/main/java/app/anull/net/team_null/MainActivity.java
+++ team-null/app/src/main/java/app/anull/net/team_null/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         settings.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                Intent intent = new Intent(MainActivity.this, NewSettingsActivity.class);
                 startActivity(intent);
             }
         });
