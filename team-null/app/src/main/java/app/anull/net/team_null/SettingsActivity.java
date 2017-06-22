7c1e7fe6c4d7c616a05cca2dfa440799100e6840
beaverdabeaver <sukuraz12@gmail.com>
2017-06-21 21:43:29 -0500

    Settings: Added buttons to layout, added action bar to activity

--- team-null/app/src/main/java/app/anull/net/team_null/SettingsActivity.java
+++ team-null/app/src/main/java/app/anull/net/team_null/SettingsActivity.java
@@ -1,5 +1,6 @@
 package app.anull.net.team_null;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,15 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        setupActionBar();
+    }
+
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Play");
+        if (actionBar != null) {
+            // Show the Up button in the action bar.
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 }
