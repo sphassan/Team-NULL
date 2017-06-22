fe3d201af8b3f642f87d8cbb7892d93a4242fe1e
sphassan <sphassan@sbcglobal.net>
2017-06-21 20:55:26 -0500

    bug fixing

--- team-null/app/src/main/java/app/anull/net/team_null/SettingsActivity.java
+++ team-null/app/src/main/java/app/anull/net/team_null/SettingsActivity.java
@@ -3,11 +3,11 @@ package app.anull.net.team_null;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SettingActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting);
+        setContentView(R.layout.activity_settings);
     }
 }
